fed783ee476122e6d6ebeea697aeb76e08698570
Michael H <107985466+mikedev23@users.noreply.github.com>
2022-07-21 16:46:53 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# 🤜👊🤛 Emoji Fighter
+
+This is a modified Emoji Figther Project from Scrimba that selects random emojis in a versus battle.
+
+## 🚀 About Me
+
+I'm an aspiring front-end developer taking courses on Scrimba with the goal of completion and certification.
+
+## 🛠️ Skills
+
+Javascript, HTML, CSS...
---
+++ app.js
@@ -0,0 +1,10 @@
+const fighters = ["🐉", "🐥", "🐊", "💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷", "🐆", "🦕", "🦁"]
+
+const stageEl = document.getElementById("stage")
+const fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function () {
+	const randomIndexOne = Math.floor(Math.random() * fighters.length)
+	const randomIndexTwo = Math.floor(Math.random() * fighters.length)
+	stageEl.textContent = `${fighters[randomIndexOne]} vs ${fighters[randomIndexTwo]}`
+})
